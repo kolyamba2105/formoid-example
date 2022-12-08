82a0a3015fd11467b062d1f901d50815eecc418f
kolyamba2105 <haravetskyy@gmail.com>
2022-12-08 02:32:54 +0200

    Setup demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import css from "./App.module.scss";
-import tools from "./data";
+import { Button, TextField } from "~/common/components";
+import { customValidator } from "~/common/utils";
+import { useForm, validator } from "formoid";
 
-export default function App() {
+function saveData(data: unknown) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(console.log(data)), 1000);
+  });
+}
+
+type FormValues = {
+  name: string;
+  password: string;
+  confirmPassword: string;
+};
+
+function App() {
+  const initialValues: FormValues = {
+    name: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const { fieldProps, handleReset, handleSubmit } = useForm({
+    initialValues,
+    validationStrategy: "onBlur",
+    validators: ({ password }) => ({
+      name: validator.sequence(
+        customValidator.nonEmptyString(),
+        validator.lengthRange(4, 64, "User name length must be between 8 and 64 chars!"),
+      ),
+      password: validator.sequence(
+        customValidator.nonEmptyString(),
+        validator.parallel(
+          validator.lengthRange(8, 64, "Password length must be between 8 and 64 chars!"),
+          validator.match(/(?=.*[A-Z])/, "Password must contain at least 1 uppercase letter!"),
+          validator.match(/(?=.*[a-z])/, "Password must contain at least 1 lowercase letter!"),
+          validator.match(/(?=.*\d)/, "Password must contain at least 1 digit!"),
+        ),
+      ),
+      confirmPassword: validator.sequence(
+        customValidator.nonEmptyString(),
+        validator.fromPredicate((confirm) => confirm === password, "Passwords do not match!"),
+      ),
+    }),
+  });
+
+  const submit = () => handleSubmit((values) => saveData(values));
+
   return (
-    <div className={css.container}>
-      <div className="flex h-72 w-72 animate-bounce flex-col justify-center rounded border border-dashed border-indigo-400 p-4 shadow-xl shadow-indigo-500/50">
-        {tools.map((tool, index) => (
-          <span
-            className="bg-gradient-to-r from-pink-400 to-sky-600 bg-clip-text text-2xl text-transparent"
-            key={index}>
-            {tool}
-          </span>
-        ))}
+    <div className="h-full w-full p-4">
+      <div className="m-auto w-[500px] space-y-3">
+        <TextField {...fieldProps("name")} placeholder="John Doe" />
+        <TextField {...fieldProps("password")} placeholder="********" type="password" />
+        <TextField {...fieldProps("confirmPassword")} placeholder="********" type="password" />
+        <div className="flex items-center justify-end space-x-2">
+          <Button color="danger" onClick={() => handleReset()} type="reset">
+            Reset
+          </Button>
+          <Button color="success" onClick={submit} type="submit">
+            Submit
+          </Button>
+        </div>
       </div>
-      <a
-        className="text-4xl text-sky-600 underline transition-all delay-100 duration-200 ease-in hover:text-sky-400"
-        href="https://github.com/kolyamba2105/webpack-template"
-        rel="noreferrer"
-        target="_blank">
-        Webpack Template
-      </a>
     </div>
   );
 }
+
+export default App;

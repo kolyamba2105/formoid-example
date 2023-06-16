923d740c27ccc3c4e74be953524c044134faa980
kolyamba2105 <haravetskyy@gmail.com>
2023-06-16 22:27:37 +0300

    Fix import

--- src/common/utils/customValidator.ts
+++ src/common/utils/customValidator.ts
@@ -1,6 +1,6 @@
-import { Validator, validator } from "formoid";
+import { validator } from "formoid";
 import { isNonEmptyString, NonEmptyString } from "./refinements";
 
-export function nonEmptyString(message?: string): Validator<string, NonEmptyString> {
+export function nonEmptyString(message?: string): validator.Validator<string, NonEmptyString> {
   return validator.fromPredicate(isNonEmptyString, message || "This field is required");
 }

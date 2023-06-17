0de15d4fb994ecb36ea18ab02dd548efff615a48
kolyamba2105 <haravetskyy@gmail.com>
2023-06-17 19:28:34 +0300

    Update example form

--- src/common/utils/customValidator.ts
+++ src/common/utils/customValidator.ts
@@ -1,6 +1,6 @@
 import { validator } from "formoid";
-import { isNonEmptyString, NonEmptyString } from "./refinements";
+import { NonEmptyString, isNonEmptyString } from "./refinements";
 
 export function nonEmptyString(message?: string): validator.Validator<string, NonEmptyString> {
-  return validator.fromPredicate(isNonEmptyString, message || "This field is required");
+  return validator.fromPredicate(isNonEmptyString, message ?? "This field is required");
 }
